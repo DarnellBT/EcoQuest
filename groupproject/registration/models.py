ba9385c69e13a5e605740a3335663254b8a8c7f9
Death008 <bam221@exeter.ac.uk>
2025-02-12 15:33:55 +0000

    Created CustomerUser table using AbstractUser from django, made migrations

--- groupproject/registration/models.py
+++ groupproject/registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    userId = models.BigAutoField(primary_key=True),
+    points = models.IntegerField(max_length=10, default=0),
+    is_user = models.BooleanField(default=1),
+    is_developer = models.BooleanField(default=0),
+    is_admin = models.BooleanField(default=0)
+
+    
