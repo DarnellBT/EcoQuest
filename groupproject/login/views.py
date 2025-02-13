c6b459e0d4282244fb581c2f2afaec24f13e51ad
Death008 <bam221@exeter.ac.uk>
2025-02-13 08:48:39 +0000

    Basic Login Page and Login Form, redirects to home if successful

--- groupproject/login/views.py
+++ groupproject/login/views.py
@@ -1,10 +1,32 @@
 from django.template import loader
-from django.shortcuts import render
-from django.http import HttpResponse, request
-from django.contrib.auth import authenticate
-from . import forms
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-def loginPage(response):
-    template = loader.get_template('loginPage.html')
-    return HttpResponse(template.render())
+def loginPage(request):
+    #load default login form
+    form = LoginForm()
+    # check if user has submitted the form
+    if request.method == 'POST':
+        #take the data submitted from the form 
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            # check whether there username and password are valid
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                #check user table against details provided to validate user
+                login(request, user)
+                return redirect('../home/')
+            else:
+                print('Authentication failed')
+        else:
+            print("Form validation failed")
+    return render(request, 'loginPage.html', {'form': form})
+
+
+        
+    
 
