cb159bdbc93a9abe0062fdccc805d3d650d770dd
DarnellBT <titredarnb@gmail.com>
2025-02-18 04:02:38 +0000

    updated registration tests to test redirect.

--- groupproject/registration/tests.py
+++ groupproject/registration/tests.py
@@ -31,21 +31,60 @@ class RegistrationViewTest(TestCase):
         """Test if the registration page loads successfully"""
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
+        # Check we get registration template.
+        self.assertTemplateUsed(response, 'registration.html')
+
+    def test_login_page_redirect_loads(self):
+        """Test if the login page loads successfully"""
+        form_data = {
+            'username': 'Dragonite',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'JohnDoe@email.com',
+            'password1': 'secret_pass',
+            'password2': 'secret_pass'
+        }
+
+        response = self.client.post('/register/', form_data)
+        login_response = self.assertRedirects(response, '/login/', status_code=302, target_status_code=200, fetch_redirect_response=True)
+        # Check we get loginPage template.
+        self.assertTemplateUsed(login_response, '../login/templates/loginPage.html')
 
 class RegistrationFormTest(TestCase):
     def test_valid_form(self):
         """Test if the form is valid"""
-        form_data = {'username': 'Dragonite', 'first_name': 'John', 'last_name': 'Doe', 'email': 'JohnDoe@email.com', 'password1': 'secret_pass', 'password2': 'secret_pass'}
+        form_data = {
+            'username': 'Dragonite',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'JohnDoe@email.com',
+            'password1': 'secret_pass',
+            'password2': 'secret_pass'
+        }
         form = RegistrationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_form(self):
         """Test if an invalid form is rejected"""
         # Checks that if all fields are empty, the form should not be valid
-        form_data = {'username': '', 'first_name': '', 'last_name': '', 'email': '', 'password1': '', 'password2': ''}
+        form_data = {
+            'username': '',
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'password1': '',
+            'password2': ''
+        }
         form = RegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
         # Checks that if passwords do not match, the form should not be valid.
-        form_data = {'username': 'Dragonite', 'first_name': 'John', 'last_name': 'Doe', 'email': 'JohnDoe@email.com', 'password1': 'secret01', 'password2': 'secret02'}
+        form_data = {
+            'username': 'Dragonite',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'JohnDoe@email.com',
+            'password1': 'secret01',
+            'password2': 'secret02'
+        }
         form = RegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
