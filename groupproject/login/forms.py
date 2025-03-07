146161f9b51dde8aa29bd28749b53d33d36e5459
bhask <bam221@exeter.ac.uk>
2025-03-07 15:56:01 +0000

    Fix login page code

--- groupproject/login/forms.py
+++ groupproject/login/forms.py
@@ -3,3 +3,8 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
     username = forms.EmailField(label='Email', max_length=254)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder':'Email'})
+        self.fields['password'].widget.attrs.update({'placeholder':'Password'})
--- groupproject/mysite/settings.py
+++ groupproject/mysite/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Email Validation
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # Keep the default backend
-    'groupproject.login.backends.EmailBackend',   # Add the custom email backend
+    'login.backends.EmailBackend',   # Add the custom email backend
 ]
 
 # Internationalization
